30833a1d86b0c3fb31d0cdf761d1f41321b53c7b
Atle Frenvik Sveen <atle.sveen@bouvet.no>
2014-11-21 11:45:07 +0100

    add stuff

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            core: {
+                src: [
+                    'src/ssrjson.js'
+                ],
+                dest: 'dist/ssrjson.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy HH:MM") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/ssrjson.min.js': ['<%= concat.core.dest %>']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
